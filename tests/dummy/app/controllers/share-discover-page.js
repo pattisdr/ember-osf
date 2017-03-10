70afcdee891009341d74be3f12165d39377c0bf9
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-03-10 15:42:27 -0500

    Modify usage of discover-page component in dummy app.

--- tests/dummy/app/controllers/share-discover-page.js
+++ tests/dummy/app/controllers/share-discover-page.js
@@ -1,29 +1,43 @@
 import Ember from 'ember';
 
-let filterQueryParams = ['tags', 'sources', 'publishers', 'funders', 'institutions', 'organizations', 'language', 'contributors', 'type'];
+let filterQueryParams = ['provider', 'subject', 'tags', 'sources', 'publishers', 'funders', 'institutions', 'organizations', 'language', 'contributors', 'type'];
 
 export default Ember.Controller.extend({
+    contributors: '',
+    detailRoute: 'nodes.detail',
+    discoverHeader: 'Demonstration of discover-page component',
+    end: '',
+    i18n: Ember.inject.service(),
+    institutions: '',
+    filterMap: {
+        providers: 'sources',
+        subjects: 'subjects'
+    },
+    filterReplace: {
+        'Open Science Framework': 'OSF',
+        'Cognitive Sciences ePrint Archive': 'Cogprints',
+        OSF: 'OSF',
+        'Research Papers in Economics': 'RePEc'
+    },
+    funders: '',
+    language: '',
+    lockedParams: {types: 'preprint'},
+    organizations: '',
+    page: 1,
+    provider: '',
+    publishers: '',
+    q: '',
     queryParams:  Ember.computed(function() {
         let allParams = ['q', 'start', 'end', 'sort', 'page'];
         allParams.push(...filterQueryParams);
         return allParams;
     }),
-
-    pageHeader: 'Demonstration of ember-share-search-component',
-
-    q: '',
-    tags: '',
-    sources: '',
-    publishers: '',
-    funders: '',
-    institutions: '',
-    organizations: '',
-    language: '',
-    contributors: '',
-    start: '',
-    end: '',
-    type: '',
+    showActiveFilters: true,
     sort: '',
-
+    sources: '',
+    start: '',
+    subject: '',
+    tags: '',
+    type: '',
 });
 
